238cbca242aaa5ca193c4634482c3dc9d3fb3795
V-Meenakshi <meenakshivinjamuri04@gmail.com>
2025-05-17 23:52:30 +0530

    first commit

---
+++ src/test/java/com/blogverse/blogverse_backend/BlogverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogverse.blogverse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
